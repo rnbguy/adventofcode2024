2b4eb9d38b18f287cd92a60c689274530b5073fd
Ranadeep Biswas <mail@rnbguy.at>
2024-12-03 11:41:50 +0100

    part2

--- day3/test.ts
+++ day3/test.ts
@@ -2,8 +2,11 @@ import { assertEquals } from "@std/assert";
 import { solve1, solve2 } from "./mod.ts";
 
 Deno.test(async function testExample() {
-  const example_data_path = new URL("example.txt", import.meta.url).pathname;
-  const example_data = await Deno.readTextFile(example_data_path);
-  assertEquals(solve1(example_data), 161);
-  // assertEquals(solve2(example_data), 0);
+  const example_data_path_1 = new URL("example1.txt", import.meta.url).pathname;
+  const example_data_1 = await Deno.readTextFile(example_data_path_1);
+  assertEquals(solve1(example_data_1), 161);
+
+  const example_data_path_2 = new URL("example2.txt", import.meta.url).pathname;
+  const example_data_2 = await Deno.readTextFile(example_data_path_2);
+  assertEquals(solve2(example_data_2), 48);
 });
