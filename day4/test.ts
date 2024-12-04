75d7d0328875165420380a13d9f4676bb2106bad
Ranadeep Biswas <mail@rnbguy.at>
2024-12-04 11:47:22 +0100

    day 4

---
+++ day4/test.ts
@@ -0,0 +1,9 @@
+import { assertEquals } from "@std/assert";
+import { process_data, solve1, solve2 } from "./mod.ts";
+
+Deno.test(async function testExample() {
+  const example_data_path = new URL("example.txt", import.meta.url).pathname;
+  const example_data = await Deno.readTextFile(example_data_path);
+  assertEquals(solve1(process_data(example_data)), 18);
+  assertEquals(solve2(process_data(example_data)), 9);
+});
