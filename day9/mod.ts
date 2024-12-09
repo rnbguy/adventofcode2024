d72f5152cc6eaf07356614ed4ee47e885cdc85d3
Ranadeep Biswas <mail@rnbguy.at>
2024-12-09 11:29:43 +0100

    part 1

--- day9/mod.ts
+++ day9/mod.ts
@@ -1,12 +1,53 @@
-export function parse(data: string): string {
-  return data.trim();
+export function parse(data: string): number[] {
+  return data.trim().split("").map(Number);
 }
 
-export function solve1(data: string): number {
-  return data.length;
+export function solve1(rdata: number[]): number {
+  const data = rdata.slice();
+
+  let lId = 0;
+  let rId = Math.floor(data.length - 1) / 2;
+
+  const disk = [];
+
+  while (lId < rId) {
+    disk.push([lId, data[lId * 2]]);
+    data[lId * 2] = 0;
+    while (data[lId * 2 + 1] > 0) {
+      if (data[rId * 2] <= data[lId * 2 + 1]) {
+        // enough space
+        disk.push([rId, data[rId * 2]]);
+        data[lId * 2 + 1] -= data[rId * 2];
+        data[rId * 2] = 0;
+        rId--;
+      } else {
+        // not enough space
+        if (lId + 1 === rId) {
+          // if last round, we can just add the remaining data
+          // ..666 becomes 666..
+          disk.push([rId, data[(lId + 1) * 2]]);
+        } else {
+          disk.push([rId, data[lId * 2 + 1]]);
+        }
+        data[rId * 2] -= data[lId * 2 + 1];
+        data[lId * 2 + 1] = 0;
+      }
+    }
+    lId++;
+  }
+
+  let index = 0;
+  let count = 0;
+
+  for (const [id, idCount] of disk) {
+    count += id * ((idCount * index) + (((idCount - 1) * idCount) / 2));
+    index += idCount;
+  }
+
+  return count;
 }
 
-export function solve2(data: string): number {
+export function solve2(data: number[]): number {
   return data.length;
 }
 
