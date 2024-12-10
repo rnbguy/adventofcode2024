0fb1ed7502d6111c6bc19db7ce66f4f58cdc9040
Ranadeep Biswas <mail@rnbguy.at>
2024-12-10 11:04:14 +0100

    day 10 part 2

--- day10/mod.ts
+++ day10/mod.ts
@@ -51,18 +51,65 @@ export function solve1(data: number[][]): number {
     findTransitiveClosure(key);
   }
 
-  return valMap.entries().filter(([_, val]) => val === 0).map(([key, _]) => {
-    return Array.from(transitiveClosure.get(key)!.values()).filter((key) =>
+  return valMap.entries().filter(([_, val]) => val === 0).map(([key, _]) =>
+    Array.from(transitiveClosure.get(key)!.values()).filter((key) =>
       valMap.get(key)! === 9
-    ).length;
-  }).reduce(
+    ).length
+  ).reduce(
     (acc, x) => acc + x,
     0,
   );
 }
 
 export function solve2(data: number[][]): number {
-  return data.length * data[0].length;
+  const valMap = new Map<string, number>();
+  const adjMap = new Map<string, Array<string>>();
+
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[0].length; j++) {
+      const key = `${i},${j}`;
+      const neighbors = [];
+      for (const [di, dj] of [[1, 0], [0, 1], [-1, 0], [0, -1]]) {
+        const ni = i + di;
+        const nj = j + dj;
+        if (ni >= 0 && ni < data.length && nj >= 0 && nj < data[0].length) {
+          if (data[i][j] + 1 === data[ni][nj]) {
+            neighbors.push(`${ni},${nj}`);
+          }
+        }
+      }
+      adjMap.set(key, neighbors);
+      valMap.set(key, data[i][j]);
+    }
+  }
+
+  const uniquePathCount = new Map<string, number>();
+
+  const findUniquePathCount = (key: string) => {
+    if (valMap.get(key)! === 9) return 1;
+
+    if (uniquePathCount.has(key)) {
+      return uniquePathCount.get(key)!;
+    }
+
+    let count = 0;
+    for (const neighbor of adjMap.get(key)!) {
+      count += findUniquePathCount(neighbor);
+    }
+    return count;
+  };
+
+  for (const [key, value] of valMap.entries()) {
+    if (value !== 0) continue;
+    findUniquePathCount(key);
+  }
+
+  return valMap.entries().filter(([_, val]) => val === 0).map(([key, _]) =>
+    findUniquePathCount(key)
+  ).reduce(
+    (acc, x) => acc + x,
+    0,
+  );
 }
 
 if (import.meta.main) {
--- day10/test.ts
+++ day10/test.ts
@@ -5,5 +5,5 @@ Deno.test(async function testExample() {
   const dataPath = new URL("example.txt", import.meta.url).pathname;
   const data = parse(await Deno.readTextFile(dataPath));
   assertEquals(solve1(data), 36);
-  assertEquals(solve2(data), 64);
+  assertEquals(solve2(data), 81);
 });
