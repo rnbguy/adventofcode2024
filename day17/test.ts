3942d8c0a81046507020a0fbb5b71d538cb2ded7
Ranadeep Biswas <mail@rnbguy.at>
2024-12-17 23:09:33 +0100

    test comment

--- day17/test.ts
+++ day17/test.ts
@@ -1,9 +1,9 @@
 import { assertEquals } from "@std/assert";
-import { parse, solve1, solve2 } from "./mod.ts";
+import { parse, solve1 } from "./mod.ts";
 
 Deno.test(async function testExample() {
   const dataPath = new URL("example.txt", import.meta.url).pathname;
   const data = parse(await Deno.readTextFile(dataPath));
   assertEquals(solve1(data), "4,6,3,5,6,3,5,2,1,0");
-  assertEquals(solve2(data), 6);
+  // no test for solve2. it works only on the input.txt
 });
