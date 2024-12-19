8c01467b0f0f80d915f36934aec80e9ff4af5758
Ranadeep Biswas <mail@rnbguy.at>
2024-12-19 10:29:25 +0100

    part 2 setup

--- day19/mod.ts
+++ day19/mod.ts
@@ -6,6 +6,21 @@ class Towels {
     this.patterns = patterns;
     this.targets = targets;
   }
+
+  countDiffWays(target: string, diffWays: Map<string, number>): number {
+    if (diffWays.has(target)) return diffWays.get(target)!;
+    const ans = this.patterns.map((pattern) => {
+      if (!target.startsWith(pattern)) return 0;
+      const nextSlice = target.slice(pattern.length, target.length);
+      let count = 0;
+      if (nextSlice.length === 0) count = 1;
+      else count = this.countDiffWays(nextSlice, diffWays);
+      diffWays.set(target, count);
+      return count;
+    }).reduce((a, b) => a + b, 0);
+    diffWays.set(target, ans);
+    return ans;
+  }
 }
 
 export function parse(data: string): Towels {
@@ -16,23 +31,12 @@ export function parse(data: string): Towels {
 }
 
 export function solve1(data: Towels): number {
-  const isPossible = new Map<string, boolean>();
+  const diffWays = new Map<string, number>();
 
-  function utilRec(target: string): boolean {
-    if (isPossible.has(target)) return isPossible.get(target)!;
-    for (const pattern of data.patterns) {
-      if (!target.startsWith(pattern)) continue;
-      const nextSlice = target.slice(pattern.length, target.length);
-      let isNextPossible = false;
-      if (nextSlice.length === 0) isNextPossible = true;
-      else isNextPossible = utilRec(nextSlice);
-      isPossible.set(target, isNextPossible);
-      if (isNextPossible) return true;
-    }
-    return false;
-  }
-
-  return data.targets.filter((target) => utilRec(target)).length;
+  return data.targets.filter((target) =>
+    data.countDiffWays(target, diffWays) > 0
+  )
+    .length;
 }
 
 export function solve2(data: Towels): number {
