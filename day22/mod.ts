d7ad9b6e8c0a06128503c07dbc46d1cc21bc43af
Ranadeep Biswas <mail@rnbguy.at>
2024-12-22 08:05:57 +0100

    part 1

--- day22/mod.ts
+++ day22/mod.ts
@@ -1,12 +1,31 @@
-export function parse(data: string): number[] {
-  return data.trim().split("\n").map(Number);
+export function parse(data: string): bigint[] {
+  return data.trim().split("\n").map(BigInt);
 }
 
-export function solve1(data: number[]): number {
-  return data.length;
+function mixAndPrune(a: bigint, b: bigint): bigint {
+  return (a ^ b) % 16777216n;
 }
 
-export function solve2(data: number[]): number {
+function nextRandom(one: bigint): bigint {
+  const two = mixAndPrune(one, one << 6n);
+  const three = mixAndPrune(two, two >> 5n);
+  const four = mixAndPrune(three, three << 11n);
+  return four;
+}
+
+function nthRandom(seed: bigint, n: number): bigint {
+  let current = seed;
+  for (let i = 0; i < n; i++) {
+    current = nextRandom(current);
+  }
+  return current;
+}
+
+export function solve1(data: bigint[]): bigint {
+  return data.map((seed) => nthRandom(seed, 2000)).reduce((a, b) => a + b, 0n);
+}
+
+export function solve2(data: bigint[]): number {
   return data.length;
 }
 
--- day22/test.ts
+++ day22/test.ts
@@ -4,6 +4,6 @@ import { parse, solve1, solve2 } from "./mod.ts";
 Deno.test(async function testExample() {
   const dataPath = new URL("example.txt", import.meta.url).pathname;
   const data = parse(await Deno.readTextFile(dataPath));
-  assertEquals(solve1(data), 4);
+  assertEquals(solve1(data), 37327623n);
   assertEquals(solve2(data), 4);
 });
