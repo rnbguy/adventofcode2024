7d345f6647d2636ac87e050358b089bcefc603b5
Ranadeep Biswas <mail@rnbguy.at>
2024-12-22 07:55:28 +0100

    setup day 22

---
+++ day22/mod.ts
@@ -0,0 +1,18 @@
+export function parse(data: string): number[] {
+  return data.trim().split("\n").map(Number);
+}
+
+export function solve1(data: number[]): number {
+  return data.length;
+}
+
+export function solve2(data: number[]): number {
+  return data.length;
+}
+
+if (import.meta.main) {
+  const dataPath = new URL("input.txt", import.meta.url).pathname;
+  const data = parse(await Deno.readTextFile(dataPath));
+  console.log(solve1(data));
+  console.log(solve2(data));
+}
