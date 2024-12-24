df69471c6f845d34678080acefc18d3527a1aa85
Ranadeep Biswas <mail@rnbguy.at>
2024-12-24 11:38:10 +0100

    part 1

--- day24/mod.ts
+++ day24/mod.ts
@@ -9,6 +9,50 @@ class Device {
     this.input = input;
     this.gates = gates;
   }
+
+  compute(): bigint {
+    const values = new Map<string, number>();
+    const outputs = new Map<string, [string, string, string]>();
+
+    for (const [wire, value] of this.input) {
+      values.set(wire, value);
+    }
+
+    for (const [a, op, b, wire] of this.gates) {
+      outputs.set(wire, [a, op, b]);
+    }
+
+    const computeWire = (wire: string): number => {
+      if (!values.has(wire)) {
+        const [a, op, b] = outputs.get(wire)!;
+
+        const a_value = computeWire(a);
+        const b_value = computeWire(b);
+
+        switch (op) {
+          case "AND":
+            values.set(wire, a_value & b_value);
+            break;
+          case "OR":
+            values.set(wire, a_value | b_value);
+            break;
+          case "XOR":
+            values.set(wire, a_value ^ b_value);
+            break;
+          default:
+            break;
+        }
+      }
+      return values.get(wire)!;
+    };
+
+    return this.gates.map(([, , , wire]) => wire).filter((wire) =>
+      wire.startsWith("z")
+    ).sort().reverse().map((wire) => computeWire(wire)).reduce(
+      (acc, value) => (acc << 1n) | BigInt(value),
+      0n,
+    );
+  }
 }
 
 export function parse(
@@ -28,8 +72,8 @@ export function parse(
   );
 }
 
-export function solve1(data: Device): number {
-  return data.input.length * data.gates.length;
+export function solve1(data: Device): bigint {
+  return data.compute();
 }
 
 export function solve2(data: Device): number {
